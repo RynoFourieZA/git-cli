2d272cb188f928db597be59dca5b0242e3a7e9d7
Ryno <rjrfourie@outlook.com>
2023-06-28 14:02:55 +0200

    $commit_message

--- gitpick.sh
+++ gitpick.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Prompt the user to enter the commit hash
-read -p "Enter the commit hash to cherry-pick: " commit_hash
+# Prompt the user to enter the commit message
+read -p "Enter the commit hash to cherry-pick: " commit_message
+
+git commit -m '$commit_message'
 
 
 # Prompt the user to enter the target branch
-read -p "Enter the target branch to apply the changes: " target_branch
+#read -p "Enter the target branch to apply the changes: " target_branch
